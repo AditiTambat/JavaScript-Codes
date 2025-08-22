869dc86e2d7897afd49e12c3998479c4ea29d96f
Aditi Tambat <126448851+AditiTambat@users.noreply.github.com>
2025-08-22 21:18:15 +0530

    Update templatelit.js

--- templatelit.js
+++ templatelit.js
@@ -1,7 +1,4 @@
 
-// template literals in js are use for concattination of values
-// we pass as an argument 
-// it use ` ` this for sentence printing and ${name} for parameter and passing value in this in log .
 function sentance(name, age){
     return `My name is ${name} and I am ${age} years old .`;
     
@@ -11,3 +8,4 @@ console.log(sentance("Dikshitra",25));
 
 var intro = (sub) => `Hello , this is my Practice session of ${sub}`;
 console.log(intro("Javascript"));
+
